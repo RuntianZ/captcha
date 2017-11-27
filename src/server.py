0d413893c003323073cb3eb674076626ae082792
Runtian Zhai <RuntianZhai@Runtian-Zhais-MacBook-Air.local>
2017-11-27 17:20:36 +0800

    fix server.py

--- src/server.py
+++ src/server.py
@@ -30,6 +30,43 @@ def server_get(username, password, groupid):
     raise ServerException(resp)
 
 
+def server_view(username, password):
+    '''
+    server_view -     View the characters of a captcha image file.
+    :param username:  Username.
+    :param password:  Password.
+    :return:          The characters of a captcha file.
+
+    This function is not supposed to be called directly in order
+    to ensure that machine learning is done under supervised mode.
+    You should instead call server_attempt.
+    '''
+    url = 'http://vitas.runtianz.cn/captcha/attempt?username=' + \
+        username + '&password=' + password
+    with urllib.request.urlopen(url) as page:
+        data = page.read()
+    resp = str(data, 'utf-8')
+    if (len(resp) > 8) and ('Success' == resp[0:7]):
+        return resp[8:]
+    raise ServerException(resp)
+
+
+def error_function(result, answer):
+    '''
+    error_function - The error function.
+    :param result:   Recognition result.
+    :param answer:   Real captcha.
+    :return:         A value between 0 and 1. 1 means identical strings
+                     while 0 means totally wrong.
+
+    This function provides an interface between C code and python code.
+    '''
+    if result == answer:
+        return 1.0
+    else:
+        return 0.0
+
+
 def server_attempt(username, password, result):
     '''
     server_attempt -   Attempt to recognize a captcha file on the server/
@@ -43,12 +80,5 @@ def server_attempt(username, password, result):
     You may attempt multiple times after you call server_attempt, which means
     that you can attempt on the same captcha file more than once.
     '''
-    url = 'http://vitas.runtianz.cn/captcha/attempt?username=' + \
-          username + '&password=' + password + '&result=' + result
-    with urllib.request.urlopen(url) as page:
-        data = page.read()
-    resp = str(data, 'utf-8')
-    if (len(resp) > 8) and ('Success' == resp[0:7]):
-        return resp[8:]
-    raise ServerException(resp)
-
+    ans = server_view(username, password)
+    return error_function(result, ans)
